50b18abaaef0eab90d24322946441e9db4028a96
Rukshesh Talwar <ruktalwa@publicisgroupe.net>
2020-04-09 19:08:47 +0530

    Javascript Essentials Commit 1

---
+++ 02_04/script.js
@@ -0,0 +1,35 @@
+var date = new Date();
+document.body.innerHTML = "<h1> Today is : "+date + "</h1>"
+
+
+//NAMING CONVENTION
+//Javascript is a case sensitive language
+//Variables start with a lowercase letter
+var letter;
+var greenDuck;
+
+//Objects and classes start with Uppercase letter
+var date = new Date();
+
+//Constants are all UPPER-CASE
+const CONSTANTLYUPPERCASE;
+
+/*------------------------
+       Data Types
+--------------------------*/
+//String Data Types
+var string = "Strings are typically words and sentences";
+var mixedQuote = 'This keeps the "Quotation" marks.';
+var escQuote = "Quotes can also be \"escaped\".";
+
+//Boolean Data Types
+//Stores one or the other of the binary pair true/false
+var theSunIsWarm = true;
+var theMoonIsMadeOfCheese = false;
+
+//null data types
+//Stores the intentional absence of an object value
+var emptyInside = null;
+
+//undefined Data Type
+var justAnotherVariable; //This variable value is undefined
